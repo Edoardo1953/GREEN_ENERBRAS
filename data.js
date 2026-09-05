3efae68d5ceccec31db5c53d6bb9eceab5586abe
Edoardo Tubia <tubia.edoardo@gmail.com>
2026-09-06 00:30:49 +0200

    Remove scrollbar track from sidebar, widen menu to 320px for ample space, bump style v85

--- data.js
+++ data.js
@@ -619,7 +619,7 @@
                         "superficie":  "200/800 mq"
                     }
                 ],
-    "lastUpdated":  "05/09/2026 23:46",
+    "lastUpdated":  "06/09/2026 00:28",
     "partners":  [
                      {
                          "detention":  0.38314176245210724,
