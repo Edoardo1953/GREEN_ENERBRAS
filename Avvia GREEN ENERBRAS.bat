92610c85242122de6fda33a7e4a9baf1d9682c70
Edoardo Tubia <tubia.edoardo@gmail.com>
2026-09-06 00:56:28 +0200

    Set fixed repository path for Edge_Profile in Avvia GREEN ENERBRAS.bat

--- Avvia GREEN ENERBRAS.bat
+++ Avvia GREEN ENERBRAS.bat
@@ -8,4 +8,4 @@ powershell.exe -ExecutionPolicy Bypass -File "C:\Users\Utilisateur\Desktop\Docum
 echo.
 echo 2. Avvio dell'applicazione in corso...
 cd /d "C:\Users\Utilisateur\Desktop\Documents\GitHub\GREEN_ENERBRAS"
-start msedge --allow-file-access-from-files --user-data-dir="%~dp0Edge_Profile" "file:///C:/Users/Utilisateur/Desktop/Documents/GitHub/GREEN_ENERBRAS/index.html"
+start msedge --allow-file-access-from-files --user-data-dir="C:\Users\Utilisateur\Desktop\Documents\GitHub\GREEN_ENERBRAS\Edge_Profile" "file:///C:/Users/Utilisateur/Desktop/Documents/GitHub/GREEN_ENERBRAS/index.html"
