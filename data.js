cc84c3416b0c807bbda362608d18a6b39983917f
Edoardo Tubia <tubia.edoardo@gmail.com>
2026-09-06 12:13:12 +0200

    Aggiunto riquadro Schema Societario nella dashboard USER e unificato Capitale Versato con Quota

--- data.js
+++ data.js
@@ -619,7 +619,7 @@
                         "superficie":  "200/800 mq"
                     }
                 ],
-    "lastUpdated":  "06/09/2026 11:41",
+    "lastUpdated":  "06/09/2026 12:07",
     "partners":  [
                      {
                          "detention":  0.38314176245210724,
