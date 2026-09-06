2c71c8a75faa53cf8a9b740fd4d2219660391f0e
Edoardo Tubia <tubia.edoardo@gmail.com>
2026-09-06 09:02:43 +0200

    img 02

--- data.js
+++ data.js
@@ -619,7 +619,7 @@
                         "superficie":  "200/800 mq"
                     }
                 ],
-    "lastUpdated":  "06/09/2026 08:42",
+    "lastUpdated":  "06/09/2026 08:55",
     "partners":  [
                      {
                          "detention":  0.38314176245210724,
