ed88acc266f75cf55e1631e195001545d24ba803
Edoardo Tubia <tubia.edoardo@gmail.com>
2026-09-06 20:47:38 +0200

    carico excel

--- data.js
+++ data.js
@@ -619,7 +619,7 @@
                         "superficie":  "200/800 mq"
                     }
                 ],
-    "lastUpdated":  "06/09/2026 20:31",
+    "lastUpdated":  "06/09/2026 20:33",
     "partners":  [
                      {
                          "detention":  0.38314176245210724,
